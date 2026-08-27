677ad59736c3ad0821afd3412e9b831e970730a6
Ken Freeman <kfree108@Kens-Mac-mini.local>
2026-08-26 21:08:29 -0600

    Restore original site styling and retain geometric logo

--- script.js
+++ script.js
@@ -423,8 +423,8 @@ const revealObserver = new IntersectionObserver(
     });
   },
   {
-    threshold: 0.08,
-    rootMargin: "0px 0px 4% 0px",
+    threshold: 0.16,
+    rootMargin: "0px 0px -8% 0px",
   }
 );
 
